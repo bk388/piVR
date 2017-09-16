12d9dca5fd511fa6e62a00669694bd6a7be8acbd
B. Kasza <bk388@cam.ac.uk>
2017-09-16 15:12:02 +0000

    Makefile works, the project should be set up correctly, can finally start working on it

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,21 @@
 #include <stdio.h>
+#include "test.h"
+#include "RTIMULib.h"
 
 int main(int arc, char* args[]) {
-	printf("Hello World!\n");
+	RTIMUSettings* settings = new RTIMUSettings("RTIMULib");
+	RTIMU *imu = RTIMU::createIMU(settings);
+	RTIMU_DATA imuData;
+	if ((imu == NULL) || (imu->IMUType() == RTIMU_TYPE_NULL)) {
+        	printf("No IMU found\n");
+        	exit(1);
+	}
+	imu->IMUInit();
+	imu->setSlerpPower(0.02);
+	imu->setGyroEnable(true);
+	imu->setAccelEnable(true);
+	imu->setCompassEnable(true);
+	imu->IMURead();
+	imuData = imu->getIMUData();
+	printf("roll: %f\n", imuData.fusionPose.x());
 }
